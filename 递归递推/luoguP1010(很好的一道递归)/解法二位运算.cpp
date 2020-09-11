e8c1ff3861433c19e3b364d7618570be66bcf1b3
shenghaoNi <1159417019@qq.com>
2020-09-11 14:41:22 +0800

    add 递归递推/luoguP1010(很好的一道递归)/解法二位运算.cpp.

---
+++ 递归递推/luoguP1010(很好的一道递归)/解法二位运算.cpp
@@ -0,0 +1,56 @@
+/*
+解法二:位运算
+递归+位运算就显得非常优雅
+当然 位运算在解决大数据问题的时候也有很重要的作用
+基本思路:
+    基本上与递归差不多
+    精华在for循环部分
+    用1<<p 代表pow(2,i) 从高位开始寻找刚好小于u的值
+    用cur记录当前所处的位数
+    cur!=1 如果不是最低位 即不是2(0) 则打印 "+"
+    判断p
+    如果p>1 打印"2("  内层递归后 打印")"
+    
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+void dfs(int u){
+    int cur=0;
+    switch(u){
+        case 0: 
+            printf("2(0)");
+            break;
+        case 1:
+            printf("2");
+            break;
+        default:
+            for(int p=14;p>=0;p--){
+                if(u&(1<<p)){
+                    if(cur)
+                        printf("+");
+                    else
+                        cur++;
+                    if(p!=1&&p!=0){
+                        printf("2(");
+                        dfs(p);
+                        printf(")");
+                    }
+                    else
+                        dfs(p);
+                }
+            }
+    }
+
+    return;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    dfs(n);
+    return 0;
+}
