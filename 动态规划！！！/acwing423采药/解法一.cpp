83f34dc7c6fd1dd4059dbbcde5df1d980bc9d65f
Niii <1159417019@qq.com>
2020-10-03 20:25:44 +0800

    add 动态规划！！！/acwing423采药/解法一.cpp.

---
+++ 动态规划！！！/acwing423采药/解法一.cpp
@@ -0,0 +1,33 @@
+/*
+算是没有辜负自己的努力吧
+基本上是秒A了
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int M = 110;
+int T;
+int m;
+int t[M],v[M];
+
+int dp[110][1010];
+
+int main(){
+	cin>>T>>m;
+	
+	for(int i=1;i<=m;i++)	
+		cin>>t[i]>>v[i];
+	
+	for(int i=1;i<=m;i++){
+		for(int j=1;j<=T;j++){
+		    dp[i][j] = dp[i-1][j];
+		    if(j>=t[i])
+			    dp[i][j] = max(dp[i][j],dp[i-1][j-t[i]]+v[i]);
+		}
+	}
+	
+	cout<<dp[m][T]<<endl;
+	
+	return 0;
+}
