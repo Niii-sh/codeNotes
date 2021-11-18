3919e7515120478d8091383dc30fecfbdf0890ad
Niii <1159417019@qq.com>
2021-11-18 12:57:12 +0000

    update 000考研笔试算法/000工大重点题/2018基数排序.cpp.

--- 000考研笔试算法/000工大重点题/2018基数排序.cpp
+++ 000考研笔试算法/000工大重点题/2018基数排序.cpp
@@ -70,3 +70,68 @@ int main(){
     
     return 0;
 }
+
+
+
+/*
+    2021 11 18
+    考前练习版 
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<vector>
+#include<queue>
+using namespace std;
+
+const int N=1e5+10;
+
+void show(vector<int>q){
+    for(int i=0;i<q.size();i++)
+        printf("%d ",q[i]);
+    puts("");
+}
+
+//q为需要进行基数排序的数组
+//w表示位数 方便起见 不然要全部算一次 用于决定基数排序的趟数
+void radixSort(vector<int>&q,int w){
+    
+    //开始进行基数排序
+    int k=1; //用于每次取出各个位置的数
+    //开始基数排序的分配和收集动作
+    for(int i=0;i<w;i++){
+        queue<int>b[10];    //默认为10进制数 故需要收集0-9
+        for(int j=0;j<q.size();j++){
+            b[q[j]/k%10].push(q[j]);
+        }
+        vector<int>tmp;
+        for(int j=0;j<q.size();j++){
+            while(!b[j].empty()){
+                tmp.push_back(b[j].front());
+                cout<<b[j].front()<<endl;
+                b[j].pop();
+            }
+        }
+        
+        q=tmp;
+        k*=10;
+        show(q);
+    }
+    
+}
+
+int main(){
+    
+    vector<int>q;
+    
+    for(int i=0;i<5;i++){
+        int x;
+        cin>>x;
+        q.push_back(x);
+    }
+    
+    radixSort(q,3);
+    
+    return 0;
+}
